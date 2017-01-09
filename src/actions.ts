38f44a3d6e8281c10174f7d4c833de99c8ff4a8c
Paul Mölders <paulmoelders@gmail.com>
2017-01-09 15:03:46 +0100

    cleanup

--- src/actions.ts
+++
@@ -1,6 +0,0 @@
-import { IAction } from './reducer';
-
-export const SET_DATA = 'SET_DATA';
-export function setData(data: Array<String>): IAction {
-    return { type: SET_DATA, payload: data };
-}
