ff89c189813bd479e3502a9f6f8702806b92efd6
Paul Mölders <paulmoelders@gmail.com>
2017-01-26 15:21:35 +0100

    migrate to webpack 2.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -20,44 +20,40 @@ module.exports = {
         publicPath: "dist"
     },
 
-    // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
 
     module: {
-        loaders: [
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader"
+            },
             {
                 test: /\.tsx?$/,
                 include: path.join(__dirname, "src"),
-                loaders: ["react-hot", "ts-loader"]
+                use: ["react-hot-loader", "ts-loader"]
             },
             {
                 test: /\.css$/,
-                loader: "style!typings-for-css-modules?modules&namedExport&camelCase"
-            }
-        ],
-
-        preLoaders: [
-            {
-                test: /\.js$/,
-                loader: "source-map-loader"
+                use: [{loader: "style-loader"}, {loader: "typings-for-css-modules-loader", options: { modules: true, namedExport: true, camelCase: true }}]
             }
         ]
     },
 
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js"),
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js' }),
         new webpack.DefinePlugin({"process.env": { "NODE_ENV": JSON.stringify(process.env.NODE_ENV) } })
     ]
 };
 
 if (process.env.NODE_ENV === "production") {
     module.exports.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
+        new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
     );
 
     module.exports.resolve.alias = {
@@ -66,4 +62,5 @@ if (process.env.NODE_ENV === "production") {
     };
 } else {
     module.exports.entry.bundle.unshift("webpack/hot/only-dev-server", "webpack-dev-server/client?http://localhost:8080");
+    module.exports.plugins.push(new webpack.NamedModulesPlugin());
 }
